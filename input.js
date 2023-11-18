403fb245d9456885f2c6292b88a90dca17aeadf4
Luiza Leao <luiza.leao96@gmail.com>
2023-11-18 16:09:59 -0500

    Replace movement keys with constants.

--- input.js
+++ input.js
@@ -1,3 +1,5 @@
+const { MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY } = require('./constants');
+
 let connection;
 
 const setupInput = (conn) => {
@@ -14,16 +16,16 @@ const handleUserInput = function(key) {
   if (key === '\u0003') {
     process.exit();
   }
-  if (key === 'w') {
+  if (key === MOVE_UP_KEY) {
     connection.write("Move: up");
   }
-  if (key === 'a') {
+  if (key === MOVE_LEFT_KEY) {
     connection.write("Move: left");
   }
-  if (key === 's') {
+  if (key === MOVE_DOWN_KEY) {
     connection.write("Move: down");
   }
-  if (key === 'd') {
+  if (key === MOVE_RIGHT_KEY) {
     connection.write("Move: right");
   }
   if (key === 'g') {
