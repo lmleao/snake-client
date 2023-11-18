66c03ceeeffd012150287cdeebced7fd87e1b708
Luiza Leao <luiza.leao96@gmail.com>
2023-11-18 16:09:00 -0500

    Create file for constants.

---
+++ constants.js
@@ -0,0 +1,17 @@
+const IP = "localhost";
+const PORT = 50541;
+const PLAYER_NAME = "LML";
+const MOVE_UP_KEY = 'w';
+const MOVE_LEFT_KEY = 'a';
+const MOVE_DOWN_KEY = 's';
+const MOVE_RIGHT_KEY = 'd';
+
+module.exports = {
+  IP,
+  PORT,
+  PLAYER_NAME,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY
+};
